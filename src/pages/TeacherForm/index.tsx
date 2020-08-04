ed64e682102834ef78951255fbd885056a7c1a69
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2020-08-04 00:27:16 -0300

    adding header component

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-function TeacherForm() {
+import PageHeader from '../../components/PageHeader'
+
+import './styles.css';
+
+function TeacherList() {
   return (
-    <h1>Teacher Form</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível você quer dar aulas. " />
+    </div>
   );
 }
 
-export default TeacherForm;
+export default TeacherList;
